3b578d74ace34c790fee9586b1ed1d8a1be45bb1
YBogdan RADOI <radoi.office@gmail.com>
2023-04-04 14:45:44 +0200

    Autorise acces page Edit Question que pour auteur

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -94,6 +94,22 @@ class QuestionController extends AbstractController
     }
 
     /**
+     * @Route("/questions/edit/{slug}", name="app_question_edit")
+     */
+    public function edit(Question $question)
+    {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_REMEMBERED');
+
+        if ($question->getOwner() !== $this->getUser()) {
+            throw $this->createAccessDeniedException('You are not the owner');
+        }
+
+        return $this->render('question/edit.html.twig', [
+            'question' => $question,
+        ]);
+    }
+
+    /**
      * @Route("/questions/{slug}/vote", name="app_question_vote", methods="POST")
      */
     public function questionVote(Question $question, Request $request, EntityManagerInterface $entityManager)
